490e7fc54f967d2ce1f4b3872653970db18f2cd2
Cosmin Tanislav <demonsingur@gmail.com>
2022-09-21 10:06:15 +0530

    berlin: Add dummy sensors sub HAL
    Change-Id: Ie20ed775344990de3e16e2eefba1754d831027fb

--- device.mk
+++ device.mk
@@ -64,6 +64,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hcef.xml
 
 # Sensors
+PRODUCT_PACKAGES += \
+    sensors.berlin
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
